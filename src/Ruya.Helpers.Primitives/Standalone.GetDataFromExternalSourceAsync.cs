16b51ed4df272849979a24dcec6e5cd34054eace
Cengiz Ilerler <cilerler@gmail.com>
2018-09-12 13:23:46 -0400

    added xml helper

--- src/Ruya.Helpers.Primitives/Standalone.GetDataFromExternalSourceAsync.cs
+++ src/Ruya.Helpers.Primitives/Standalone.GetDataFromExternalSourceAsync.cs
@@ -8,37 +8,61 @@ namespace Ruya.Helpers.Primitives
 {
     public partial class Standalone
     {
-        public static async Task<string> GetDataFromExternalSourceAsync(ILogger logger, string url, CancellationToken cancellationToken = default(CancellationToken), HttpContent httpContent = null)
-        {
-            const string methodName = nameof(GetDataFromExternalSourceAsync);
-            var retry = new RetryWithExponentialBackoff(logger);
+		public static async Task<string> GetDataFromExternalSourceAsync(ILogger logger, string url, CancellationToken cancellationToken = default, HttpContent httpContent = null, bool ensureSuccessStatusCode = true)
+		{
+			const string methodName = nameof(GetDataFromExternalSourceAsync);
+			logger.LogTrace("[{MethodName}] {Url}", methodName, url);
 
-            bool usePostMethod = httpContent != null;
-            string responseBodyAsText = null;
-            using (var httpClient = new HttpClient())
-            {
-                async Task Func(CancellationToken ct)
-                {
-                    try
-                    {
-                        HttpResponseMessage response = usePostMethod
-                                                           // ReSharper disable AccessToDisposedClosure
-                                                           ? await httpClient.PostAsync(url, httpContent, ct)
-                                                           : await httpClient.GetAsync(url, ct);
-                                                           // ReSharper restore AccessToDisposedClosure
-                        response.EnsureSuccessStatusCode();
-                        responseBodyAsText = await response.Content.ReadAsStringAsync();
-                        logger.LogTrace($"[{methodName}] StatusCode {{StatusCode}} ReasonPhrase {{ReasonPhrase}} ResponseBody {{responseBodyAsText}}", response.StatusCode, response.ReasonPhrase, responseBodyAsText);
-                    }
-                    catch (OperationCanceledException oce)
-                    {
-                        logger.LogInformation($"Cancellation requested {oce.Message}");
-                    }
-                }
+			var retry = new RetryWithExponentialBackoff(logger);
 
-                await retry.RunAsync(Func, cancellationToken);
-            }
-            return responseBodyAsText;
+			bool usePostMethod = httpContent != null;
+			string responseBodyAsText = null;
+
+			async Task Func(CancellationToken ct)
+			{
+				responseBodyAsText = null;
+				using (var httpClient = new HttpClient())
+				{
+					try
+					{
+						HttpResponseMessage response = usePostMethod
+							                               ? await httpClient.PostAsync(url
+							                                                          , httpContent
+							                                                          , ct)
+							                               : await httpClient.GetAsync(url
+							                                                         , ct);
+						responseBodyAsText = await response.Content.ReadAsStringAsync();
+						logger.LogTrace("[{MethodName}] StatusCode {StatusCode} ReasonPhrase {ReasonPhrase} ResponseBody {responseBodyAsText}"
+						              , methodName
+						              , response.StatusCode
+						              , response.ReasonPhrase
+						              , responseBodyAsText);
+						if (ensureSuccessStatusCode)
+						{
+							// UNDONE once https://github.com/dotnet/corefx/issues/26684 get deployed
+							//response.EnsureSuccessStatusCode();
+							if (!response.IsSuccessStatusCode)
+							{
+								logger.LogWarning("[{MethodName}] StatusCode {StatusCode} ReasonPhrase {ReasonPhrase} ResponseBody {responseBodyAsText}"
+											, methodName
+											, response.StatusCode
+											, response.ReasonPhrase
+											, responseBodyAsText);
+							}
+						}
+					}
+					catch (OperationCanceledException oce)
+					{
+						logger.LogInformation($"Cancellation requested {oce.Message}");
+					}
+				}
+
+			}
+
+			await retry.RunAsync(Func
+			                   , cancellationToken);
+
+			return responseBodyAsText;
         }
     }
 }
