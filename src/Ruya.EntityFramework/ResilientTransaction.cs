ebff9d84e4b88d0a6abd35d1c461cdfb6864ad69
Cengiz Ilerler <cilerler@gmail.com>
2023-06-14 14:33:01 -0400

    update EntityFramework

--- src/Ruya.EntityFramework/ResilientTransaction.cs
+++ src/Ruya.EntityFramework/ResilientTransaction.cs
@@ -3,32 +3,32 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 
-namespace Ruya.EntityFramework
+namespace Ruya.EntityFramework;
+
+public class ResilientTransaction
 {
-    public class ResilientTransaction
-    {
-        private readonly DbContext _dbContext;
+	private readonly DbContext _dbContext;
 
-        private ResilientTransaction(DbContext dbContext)
-        {
-            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
-        }
+	private ResilientTransaction(DbContext dbContext)
+	{
+		_dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+	}
 
-        public static ResilientTransaction New(DbContext context) => new ResilientTransaction(context);
+	public static ResilientTransaction New(DbContext context)
+	{
+		return new ResilientTransaction(context);
+	}
 
-        public async Task ExecuteAsync(Func<Task> action)
-        {
-            //Use of an EF Core resiliency strategy when using multiple DbContexts within an explicit BeginTransaction():
-            //See: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
-            IExecutionStrategy strategy = _dbContext.Database.CreateExecutionStrategy();
-            await strategy.ExecuteAsync(async () =>
-                                        {
-                                            using (IDbContextTransaction transaction = _dbContext.Database.BeginTransaction())
-                                            {
-                                                await action();
-                                                transaction.Commit();
-                                            }
-                                        });
-        }
-    }
+	public async Task ExecuteAsync(Func<Task> action)
+	{
+		//Use of an EF Core resiliency strategy when using multiple DbContexts within an explicit BeginTransaction():
+		//See: https://docs.microsoft.com/en-us/ef/core/miscellaneous/connection-resiliency
+		IExecutionStrategy strategy = _dbContext.Database.CreateExecutionStrategy();
+		await strategy.ExecuteAsync(async () =>
+		{
+			await using IDbContextTransaction transaction = await _dbContext.Database.BeginTransactionAsync();
+			await action();
+			await transaction.CommitAsync();
+		});
+	}
 }
