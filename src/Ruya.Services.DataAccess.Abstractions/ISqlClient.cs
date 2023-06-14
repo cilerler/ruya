2dbb5e5f2deb05c23a57d9387c6d2f3079819663
Cengiz Ilerler <cilerler@gmail.com>
2023-06-14 16:08:34 -0400

    update DataAccess

--- src/Ruya.Services.DataAccess.Abstractions/ISqlClient.cs
+++ src/Ruya.Services.DataAccess.Abstractions/ISqlClient.cs
@@ -2,20 +2,25 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 
-namespace Ruya.Services.DataAccess.Abstractions
+namespace Ruya.Services.DataAccess.Abstractions;
+
+public interface ISqlClient
 {
-    public interface ISqlClient
-    {
-        int DefaultTimeout { get; set; }
-        string ConnectionStringKey { get; set; }
-        string ConnectionString { get; }
-        void Query(Action<SqlConnection> action);
-        int Execute(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default(int));
-        IEnumerable<T> Query<T>(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default(int));
-        int TransactionalExecute(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default(int));
-        IEnumerable<T> TransactionalQuery<T>(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default(int));
-        IEnumerable<T> TransactionalQueryReadUncommitted<T>(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default(int));
-        void BulkInsert<T>(string tableName, IEnumerable<T> items, string[] members, SqlConnection alternateConnection = null, SqlTransaction scope = null, int commandTimeout = default(int));
-        void BulkInsertExecute<T>(string tableName, IEnumerable<T> items, string[] members, SqlConnection connection, SqlTransaction scope = null, int commandTimeout = default(int));
-    }
+	int DefaultTimeout { get; set; }
+	string ConnectionStringKey { get; set; }
+	string ConnectionString { get; }
+	void Query(Action<SqlConnection> action);
+	int Execute(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default);
+	IEnumerable<T> Query<T>(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default);
+	int TransactionalExecute(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default);
+	IEnumerable<T> TransactionalQuery<T>(string sql, object parameters, bool isStoredProcedure = false, int commandTimeout = default);
+
+	IEnumerable<T> TransactionalQueryReadUncommitted<T>(string sql, object parameters, bool isStoredProcedure = false,
+		int commandTimeout = default);
+
+	void BulkInsert<T>(string tableName, IEnumerable<T> items, string[] members, SqlConnection alternateConnection = null,
+		SqlTransaction scope = null, int commandTimeout = default);
+
+	void BulkInsertExecute<T>(string tableName, IEnumerable<T> items, string[] members, SqlConnection connection, SqlTransaction scope = null,
+		int commandTimeout = default);
 }
