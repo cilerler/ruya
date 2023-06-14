21ab49d8ae394e3efb0691ac2ec02d4da0fd7ecd
Cengiz Ilerler <cilerler@gmail.com>
2023-06-14 09:29:26 -0400

    optimize

--- src/Ruya.Primitives/ControlChars.cs
+++ src/Ruya.Primitives/ControlChars.cs
@@ -1,56 +1,55 @@
-namespace Ruya.Primitives
+namespace Ruya.Primitives;
+
+public static class ControlChars
 {
-    public static class ControlChars
-    {
-        /// <summary>
-        ///     Chr(8)
-        /// </summary>
-        public const char BackSpace = '\b';
+	/// <summary>
+	///     Chr(8)
+	/// </summary>
+	public const char BackSpace = '\b';
 
-        /// <summary>
-        ///     Chr(13)
-        /// </summary>
-        public const char CarriageReturn = '\r';
+	/// <summary>
+	///     Chr(13)
+	/// </summary>
+	public const char CarriageReturn = '\r';
 
-        /// <summary>
-        ///     Chr(10)
-        /// </summary>
-        public const char LineFeed = '\n';
+	/// <summary>
+	///     Chr(10)
+	/// </summary>
+	public const char LineFeed = '\n';
 
-        /// <summary>
-        ///     Chr(13) & Chr(10)
-        /// </summary>
-        public const string CarriageReturnLineFeed = "\r\n";
+	/// <summary>
+	///     Chr(13) & Chr(10)
+	/// </summary>
+	public const string CarriageReturnLineFeed = "\r\n";
 
-        /// <summary>
-        ///     Chr(13) & Chr(10) (same as CarriageReturnLineFeed)
-        /// </summary>
-        public const string NewLine = CarriageReturnLineFeed;
+	/// <summary>
+	///     Chr(13) & Chr(10) (same as CarriageReturnLineFeed)
+	/// </summary>
+	public const string NewLine = CarriageReturnLineFeed;
 
-        public const char FormFeed = '\f';
+	public const char FormFeed = '\f';
 
-        /// <summary>
-        ///     Chr(0)
-        /// </summary>
-        public const char Null = '\0';
+	/// <summary>
+	///     Chr(0)
+	/// </summary>
+	public const char Null = '\0';
 
-        public const char Alert = '\a';
-        public const char Quote = '\"';
-        public const char QuoteSingle = '\'';
+	public const char Alert = '\a';
+	public const char Quote = '\"';
+	public const char QuoteSingle = '\'';
 
-        /// <summary>
-        ///     Chr(9)
-        /// </summary>
-        public const char Tab = '\t';
+	/// <summary>
+	///     Chr(9)
+	/// </summary>
+	public const char Tab = '\t';
 
-        /// <summary>
-        ///     Chr(11)
-        /// </summary>
-        public const char VerticalTab = '\v';
+	/// <summary>
+	///     Chr(11)
+	/// </summary>
+	public const char VerticalTab = '\v';
 
-        public const char BackSlash = '\\';
-        public const char Space = ' ';
+	public const char BackSlash = '\\';
+	public const char Space = ' ';
 
-        public const char Comma = ',';
-    }
+	public const char Comma = ',';
 }
--- src/Ruya.Primitives/HashCode.cs
+++ src/Ruya.Primitives/HashCode.cs
@@ -1,32 +1,32 @@
+using System;
 using System.Collections.Generic;
 
-namespace Ruya.Primitives
+namespace Ruya.Primitives;
+
+public struct HashCode
 {
-	public struct HashCode
-    {
-        private readonly int _value;
+	private readonly int _value;
 
-        public HashCode(int value)
-        {
-            _value = value;
-        }
+	public HashCode(int value)
+	{
+		_value = value;
+	}
 
-        public static HashCode Start { get; } = new HashCode(17);
+	public static HashCode Start { get; } = new(17);
 
-        public static implicit operator int(HashCode hash)
-        {
-            return hash._value;
-        }
+	public static implicit operator int(HashCode hash)
+	{
+		return hash._value;
+	}
 
-        public HashCode Hash<T>(T obj)
-        {
-            int h = EqualityComparer<T>.Default.GetHashCode(obj);
-            return unchecked(new HashCode(_value * 31 + h));
-        }
+	public readonly HashCode Hash<T>(T obj)
+	{
+		int h = EqualityComparer<T>.Default.GetHashCode(obj ?? throw new ArgumentNullException(nameof(obj)));
+		return unchecked(new HashCode(_value * 31 + h));
+	}
 
-        public override int GetHashCode()
-        {
-            return _value;
-        }
-    }
+	public override int GetHashCode()
+	{
+		return _value;
+	}
 }
